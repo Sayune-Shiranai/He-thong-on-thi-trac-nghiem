eac10118baac23e27924b7ccaa4caa7da01f8cdc
Sayune Shiranai <sayuneshiranai1404@gmail.com>
2026-05-15 03:43:00 +0700

    fix register

--- backend/controllers/RegisterController.js
+++ backend/controllers/RegisterController.js
@@ -9,35 +9,90 @@ const bcrypt = require('bcrypt');
 const Register = async (req, res) => {
   const { username, email, password, confirmPassword } = req.body;
 
-  if (!username)
-    return res.status(400).json({ field: "username", message: "Tên đăng nhập không được để trống!" });
+  if (!username) {
+    return res.status(400).json({ 
+      field: "username", 
+      message: "Tên đăng nhập không được để trống!" 
+    });
+  }
 
-  if (!email)
-    return res.status(400).json({ field: "email", message: "Email không được để trống!" }); 
+  if (!email) {
+    return res.status(400).json({ 
+      field: "email", 
+      message: "Email không được để trống!" 
+    });
+  }
 
-  if (!password)
-      return res.status(400).json({ field: "password", message: "Mật khẩu không được để trống!" });
+  if (!password) {
+    return res.status(400).json({ 
+      field: "password", 
+      message: "Mật khẩu không được để trống!" 
+    });
+  }
 
-  if (password.length < 6)
-      return res.status(400).json({ field: "password", message: "Mật khẩu phải có ít nhất 6 ký tự!" });
+  if (password.length < 6) {
+    return res.status(400).json({ 
+      field: "password", 
+      message: "Mật khẩu phải có ít nhất 6 ký tự!" 
+    });
+  }
 
-  if (!confirmPassword)
-      return res.status(400).json({ field: "confirmPassword", message: "Xác nhận mật khẩu không được để trống!" });
+  if (!confirmPassword) {
+    return res.status(400).json({ 
+      field: "confirmPassword", 
+      message: "Xác nhận mật khẩu không được để trống!" 
+    });
+  }
 
-  if (password !== confirmPassword)
-      return res.status(400).json({ field: "confirmPassword", message: "Nhập lại mật khẩu không khớp!" });
+  if (password !== confirmPassword) {
+    return res.status(400).json({ 
+      field: "confirmPassword", 
+      message: "Nhập lại mật khẩu không khớp!" 
+    });
+  }
 
   const checkEmail = await db.User.findOne({ where: { email } });
-  if (checkEmail)
-    return res.status(400).json({ field: "email", message: "Email đã tồn tại!" });
+  if (checkEmail) {
+    return res.status(400).json({ 
+      field: "email", 
+      message: "Email đã tồn tại!" 
+    });
+  }
 
   const checkUser = await db.User.findOne({ where: { username } });
-  if (checkUser)
-    return res.status(400).json({ field: "username", message: "Tài khoản đã tồn tại!" });
+  if (checkUser) {
+    return res.status(400).json({ 
+      field: "username", 
+      message: "Tài khoản đã tồn tại!" 
+    });
+  }
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const role = await db.Role.findOne({ where: { name: 'Student' } });
+  if (!role) {
+    return res.status(400).json({
+      field: "role",
+      message: "Vui lòng chọn vai trò!"
+    });
+  }
+
+  if (!["Student", "Teacher"].includes(role)) {
+    return res.status(400).json({
+      field: "role",
+      message: "Vai trò không hợp lệ!"
+    });
+  }
+
+  const roleData = await db.Role.findOne({
+    where: { name: role }
+  });
+
+  if (!roleData) {
+    return res.status(400).json({
+      field: "role",
+      message: "Role không tồn tại!"
+    });
+  }
 
   const status = await db.Status.findOne({ where: { name: 'Approved' } });
 
@@ -45,7 +100,7 @@ const Register = async (req, res) => {
     username, 
     email, 
     password: hashedPassword,
-    role_id: role.id,
+    role_id: roleData.id,
     status_id: status.id
   });
     return res.status(201).json({ 
