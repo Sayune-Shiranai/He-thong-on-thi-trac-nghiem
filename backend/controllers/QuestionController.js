fb73fe259753f3907bd87234be759652c2d8386b
Sayune Shiranai <sayuneshiranai1404@gmail.com>
2026-05-15 03:27:25 +0700

    fix question

--- backend/controllers/QuestionController.js
+++ backend/controllers/QuestionController.js
@@ -36,50 +36,48 @@ const CreateQuestion = async (req, res) => {
 const UploadQuestionImage = async (req, res) => {
   try {
     const {
+      content_img,
       answer_count,
       correct_answers
     } = req.body;
 
-    // kiểm tra ảnh
     if (!req.file) {
       return res.status(400).json({
         message: "Vui lòng upload ảnh"
       });
     }
-    // kiểm tra số lượng câu
+
+    //dùng nếu gửi form-data ko chuyển sang kiểu int
+    // const totalQuestions = parseInt(answer_count);
+    
     if (!answer_count || answer_count <= 0) {
       return res.status(400).json({
         message: "Số lượng câu hỏi không hợp lệ"
       });
     }
-    // parse mảng nếu gửi form-data
-    const parsedAnswers =
-      typeof correct_answers === "string"
+
+    const parsedAnswers = typeof correct_answers === "string"
         ? JSON.parse(correct_answers)
         : correct_answers;
-    // kiểm tra mảng đáp án đúng
-    if (!Array.isArray(parsedAnswers)) {
+
+        if (!Array.isArray(parsedAnswers)) {
       return res.status(400).json({
         message: "correct_answers phải là mảng"
       });
     }
-    // kiểm tra số lượng đáp án đúng
+
     if (parsedAnswers.length != answer_count) {
       return res.status(400).json({
         message: "Số lượng đáp án đúng không khớp"
       });
     }
+
     const createdQuestions = [];
-    // tạo nhiều câu hỏi
+
     for (let i = 0; i < answer_count; i++) {
       const answer = parsedAnswers[i];
-      // validate đáp án đúng
-      if (!["A", "B", "C", "D"].includes(answer)) {
-        return res.status(400).json({
-          message: `Đáp án câu ${i + 1} không hợp lệ`
-        });
-      }
       const question = await db.Question.create({
+        content_img,
         image: `/uploads/questions/${req.file.filename}`,
         question_number: i + 1,
         correct_answer: answer
@@ -88,7 +86,6 @@ const UploadQuestionImage = async (req, res) => {
       createdQuestions.push(question);
     }
     return res.status(201).json({
-
       message: "Upload ảnh thành công",
       total_questions: createdQuestions.length,
       data: createdQuestions
