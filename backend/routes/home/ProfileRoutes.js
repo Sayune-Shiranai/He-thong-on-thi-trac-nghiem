c420a61b860eb6a3aeaa815623da4b880d1904ee
akdie2004 <akdie2082004@gmail.com>
2026-05-26 01:52:10 +0700

    profileroutes

---
+++ backend/routes/home/ProfileRoutes.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  GetProfile,
+  UpdateProfile,
+  ChangePassword,
+  UploadAvatar
+} = require('../controllers/ProfileController');
+
+const upload = require('../middlewares/upload');
+
+// lấy profile
+router.get('/:id', GetProfile);
+
+// cập nhật profile
+router.put('/update/:id', UpdateProfile);
+
+// đổi mật khẩu
+router.put('/change-password/:id', ChangePassword);
+
+// upload avatar
+router.post(
+  '/upload-avatar/:id',
+  upload.single('avatar'),
+  UploadAvatar
+);
+
+module.exports = router;
