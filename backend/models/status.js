b87957ce4e2106ee66ec716fbacca4bc0c607b3e
Sayune Shiranai <sayuneshiranai1404@gmail.com>
2026-06-07 10:15:20 +0700

    fix status db

--- backend/models/status.js
+++ backend/models/status.js
@@ -6,6 +6,8 @@ module.exports = (sequelize, DataTypes) => {
   class Status extends Model {
     static associate(models) {
       Status.hasMany(models.User, { foreignKey: 'status_id' });
+      Status.hasMany(models.Exam, { foreignKey: 'status_id' });
+      Status.hasMany(models.Question, { foreignKey: 'status_id' });
     }
   }
   Status.init({
